9f1a531ff43c2a4b64fcb6900df6d5f4abc0ed53
madocho <smcho@hanbit.co.kr>
2018-03-13 17:04:32 +0900

    Add files via upload

---
+++ xml_mysql_insert.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+BASE_URL = "http://www.yes24.com/_par_/Rss/KNU001001001.xml"
+
+responses = requests.get(BASE_URL)
+soup = BeautifulSoup(responses.content, 'lxml-xml')
+
+# 책 제목, 도서코드 찾기
+for bookitem in soup.findAll('item'):
+  booktitle = bookitem.find('title')
+  guid = bookitem.find('guid')
+
+import pymysql
+
+# mysql 연결
+conn = pymysql.connect(host="localhost", user="root", password="12270912", db="book_infor", charset="utf8")
+
+curs = conn.cursor(pymysql.cursors.DictCursor)
+
+data = (guid.text, booktitle.text)
+
+# sql 데이터 입력
+sql = """insert into book_test(bookcode,bookname)
+       values (%s, %s)"""
+
+curs.executemany(sql, [data])
+
+conn.commit()
+
+conn.close()
